445ef125bbfb8a46852e7cdd2df0f05181c4e7ef
pseudonym55 <bluemagnusstormcrow@gmail.com>
2020-03-20 13:52:54 +0000

    s

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ public class Main {
 	
 	public static void main(String[] args) {
 		ProjectListGenerator generatePL = new ProjectListGenerator("src/Miskatonic Staff Members.csv");
-		generatePL.generateProjectList("src/projectList.csv", 250);
+		ProjectListGenerator generatePrefList = new ProjectListGenerator("src/src/names.csv","src/projectList.csv" );
+		generatePL.generateProjectList("src/projectList60.csv", 30);
+		generatePL.generateProjectList("src/projectList120.csv", 60);
+		generatePL.generateProjectList("src/projectList240.csv", 120);
+		generatePL.generateProjectList("src/projectList500.csv", 250);
+		
 	}
 }
