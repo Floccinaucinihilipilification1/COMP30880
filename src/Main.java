4ad3cd7473ea860a81d250f8da7bc646c34a147f
pseudonym55 <bluemagnusstormcrow@gmail.com>
2020-03-20 14:37:50 +0000

    Mostly combined. Has null pointer error.

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,9 @@ public class Main {
 	
 	public static void main(String[] args) {
 		ProjectListGenerator generatePL = new ProjectListGenerator("src/Miskatonic Staff Members.csv");
-		ProjectListGenerator generatePrefList = new ProjectListGenerator("src/src/names.csv","src/projectList.csv" );
-		generatePL.generateProjectList("src/projectList60.csv", 30);
-		generatePL.generateProjectList("src/projectList120.csv", 60);
-		generatePL.generateProjectList("src/projectList240.csv", 120);
-		generatePL.generateProjectList("src/projectList500.csv", 250);
+		generatePL.generateProjectList("src/projectList.csv", 30);
+		StudentGenerator generatePrefList = new StudentGenerator();
+		generatePrefList.StudentPrefsGenerator("src/names.csv","src/projectList.csv" );
 		
 	}
 }
