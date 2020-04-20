b04d49bb1a0755ce69a50884e8d6447eab19f879
mbradley97 <35834463+mbradley97@users.noreply.github.com>
2020-04-20 01:10:03 +0100

    Add files via upload
    Simulated Annealing implemented

---
+++ src/HillClimbingWithSA.java
@@ -0,0 +1,66 @@
+import java.util.Random;
+import java.math.*;
+public class HillClimbingWithSA {
+
+	public static void climbing(CandidateSolution candidateSolution)
+	{
+	int x = 0;
+	double temp = 100;
+	double cooling = 10;
+	boolean used = false;
+	while (x < 1000)  // run until 1000 worse results are obtained in a row
+	{	
+		
+		double y = SimulatedAnnealing.Energy(candidateSolution);
+		System.out.println("\n" + "The current fitness of the solution is " + SimulatedAnnealing.Fitness(candidateSolution));
+		System.out.println("\n" + "The current energy of the solution is" + SimulatedAnnealing.Energy(candidateSolution));
+		Random rand = new Random();
+		int studentIndex = rand.nextInt(candidateSolution.getSize());
+		int newRank = rand.nextInt(10);
+		int oldRank = candidateSolution.getSolutionRankAt(studentIndex);
+		candidateSolution.setSolutionRankAt(studentIndex, newRank);
+		double z = SimulatedAnnealing.Energy(candidateSolution);
+				
+		if(z < y) {
+			x = 0;
+			if (1 < temp) temp = temp - cooling;
+		} 
+			
+		else if (y < z)
+		{
+
+			double energyIncrease = z - y;
+			double boltzmannProb = 1 / Math.pow(Math.E,(energyIncrease/temp));
+			if ( rand.nextDouble() < boltzmannProb) {
+				x = 0;
+				if (1 < temp) temp = temp - cooling;
+			}
+			else {
+				candidateSolution.setSolutionRankAt(studentIndex, oldRank); // revert worse choices.	
+				x++;
+			}
+		}
+		
+		
+		System.out.println("Student: " + candidateSolution.getStudentAt(studentIndex).getName() + ", Rank changed from: " + oldRank + " to: " + newRank + "\n");
+		
+	
+		
+		
+		
+		
+		
+
+	
+	}
+	
+	}
+	
+	
+		
+
+	
+
+	
+	
+}
