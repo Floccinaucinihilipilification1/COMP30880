e278e169389d3b1f0a2f0170fc285c0665bb9d20
mbradley97 <35834463+mbradley97@users.noreply.github.com>
2020-05-14 06:33:19 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@ import javax.swing.JFrame;
 
 public class Main {
 	public static void main(String[] args) {
-		//CandidateSolutionGenerator cand = new CandidateSolutionGenerator("src/pref.csv");
-		//CandidateSolution sol = cand.generate();
-		//System.out.println(sol.toString());
 		JFrame UI = new GUI();
 		UI.setVisible(true);
 	}
