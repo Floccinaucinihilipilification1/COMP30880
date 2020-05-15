5e34d5b8b8218e4221ecb251ed6fe8e8086c0e9c
arcanass <pentakronox@gmail.com>
2020-05-15 03:08:36 +0100

    new textarea

--- src/GUI.java
+++ src/GUI.java
@@ -4,13 +4,13 @@ import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.awt.BorderLayout;
-import java.awt.Font;
-import java.awt.FlowLayout;
 import java.io.BufferedReader;
 import java.awt.event.ItemEvent;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.Random;
+
 
 
 public class GUI extends JFrame{
@@ -28,10 +28,11 @@ public class GUI extends JFrame{
 	static JPanel jp = new JPanel(new BorderLayout(20,20));
 	static JTextField jt = new JTextField("Enter your choice here",60);
 	static JTextArea ja = new JTextArea(10, 10);
+	static JTextArea jb = new JTextArea(10, 10);
 	static File file;
 	static String input;
 	
-	//Finished: putting status bar, font
+	//Finished: putting status bar
 	//TODO: put some frame color(Menubar, buttons).
 
 	
@@ -56,6 +57,7 @@ public class GUI extends JFrame{
 
 	    private static void text(CandidateSolution solution) {
 	    jp.add(jt, BorderLayout.SOUTH);
+	    
 	    
 	    jt.addActionListener(new ActionListener() {
 
@@ -83,7 +85,8 @@ public class GUI extends JFrame{
 	     //   iconSave = new ImageIcon("src/save.png");
 	        iconExit = new ImageIcon("src/exit.png");
 	        JFileChooser fileChooser = new JFileChooser(); //This method will search all type of files, not only CSV/TSV
-	        jp.add(ja, BorderLayout.NORTH); //This adds text area to GUI
+	        jp.add(ja, BorderLayout.WEST); //This adds text area to GUI
+	        jp.add(jb, BorderLayout.EAST);
 	        JScrollPane scrollPane1 = new JScrollPane(ja);
 	        scrollPane1.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 	        ja.setEditable(false);
@@ -117,11 +120,10 @@ public class GUI extends JFrame{
 	        				ja.setText(ja.getText() + "\n" + line);
 		        		    line = input.readLine();
 	        			}
-	        			ja.append("\n\n" + "The following preference list has been loaded: " + file.getAbsolutePath() + "\n");
+	        			ja.append("\n\n\n" + "The following preference list has been loaded: " + file.getAbsolutePath() + "\n");
 	        			ja.append("The number of lines in the preference file loaded is " + lines + "\n");
 	        			GUI.cand = new CandidateSolutionGenerator(y);
 	        			input.close();
-	        			menuDisplay();
 	        		}
 	        		catch (Exception e) {
 	        			e.printStackTrace();
@@ -131,7 +133,6 @@ public class GUI extends JFrame{
 		        		ja.append("Opening failed" + "\n");
         		}
 	        		 if(!fileType.equals(n) && !fileType.equals(m) ) {ja.append("\n" + "The file loaded was not a tsv or a csv file. Please load a tsv or csv file "+ "\n");
-	        		 menuDisplay();
 	        		 }
 	        }}
 	       
@@ -211,7 +212,6 @@ public class GUI extends JFrame{
 			 GUI.solution = cand.generate();
 			   ja.append("\n You typed 1. Solution Generated \n");
 			   ja.append("The size of the solution is" + GUI.solution.getSize() + "\n");
-			   menuDisplay();
                
 			   break; }
 			
@@ -220,7 +220,7 @@ public class GUI extends JFrame{
 			 else {
 				 ja.append("\n" + GUI.solution.toString());
 				 ja.append("\n You typed 2. Solution Printed. \n");
-			 menuDisplay();
+		
 				break; }
 			
 			
@@ -228,10 +228,10 @@ public class GUI extends JFrame{
 			if(GUI.solution == null) {ja.append("\n No solution generated. Please generate a solution first \n"); break;}
 			 else {
 			ja.append("\n You typed 3.\n");
-			ja.append("\n" + "The Fitness of the solution is " + SolutionChanger.Fitness(GUI.solution));
+			ja.append("The Fitness of the solution is " + SolutionChanger.Fitness(GUI.solution));
 			ja.append("\n" + "The Energy of the solution is " + SolutionChanger.Energy(GUI.solution)+ "\n");
 			
-			menuDisplay();
+	
 				break; }
 			
 		case 4:
@@ -241,7 +241,7 @@ public class GUI extends JFrame{
 			ja.append("\n You typed 4. Random change applied to solution. \n");
 			ja.append("The Energy of the solution is: " + SolutionChanger.Energy(GUI.solution)+ "\n");
 
-			menuDisplay();
+			
              break; }
           
 		case 5:
@@ -252,7 +252,8 @@ public class GUI extends JFrame{
 			ja.append("\n You typed 5. Hill Climbing implemented to solution. \n");
 			ja.append("Hill Climbing solution is saved at C:\\Users\\Public\\OptimumSolutionHillClimbing.csv \n");
 			ja.append("The Energy of the solution is " + SolutionChanger.Energy(GUI.solution)+ "\n");
-			menuDisplay();
+			ja.append(SolutionChanger.changeRandom(GUI.solution));
+		
              break; }
 			
 			
@@ -264,7 +265,7 @@ public class GUI extends JFrame{
 				 ja.append("\n You typed 6. Simulated Annealing implemented to solution. \n");
 				 ja.append("SA solution is saved at C:\\Users\\Public\\OptimumSolutionSA.csv"+ "\n");
 				 ja.append("The Energy of the solution is " + SolutionChanger.Energy(GUI.solution)+ "\n");
-			menuDisplay(); 
+			
 			    break; }
 			
 		case 7:
@@ -275,14 +276,14 @@ public class GUI extends JFrame{
 				 ja.append("\n You typed 7. Genetic Annealing implemented to solution. \n");
 				 ja.append("GA solution is saved at C:\\Users\\Public\\OptimumSolutionGA.csv"+ "\n");
 				 ja.append("The fitness of the solution is " + SolutionChanger.Fitness(GUI.solution)+ "\n");
-			menuDisplay();
+			
 			    break; }
 	    
 		case 8:
 			if(GUI.file == null) {ja.append("\n" + "No files have been loaded"  + "\n"); break;}
 			 else {
 			ja.append("\n" + "The following preference list has been loaded" + GUI.file.getAbsolutePath() + "\n");	 
-			menuDisplay();
+			
 			    break; }
 			
 		case 9:
@@ -291,13 +292,13 @@ public class GUI extends JFrame{
 				 GUI.solution.saveSolution("C:\\Users\\Public\\OptimumSolution.csv"); 
 				 ja.append("\n" + "File Saved"  + "\n");
 				 ja.append("\n" + "The file has been saved to the following location C:\\Users\\Public\\OptimumSolution.csv "  + "\n");
-			menuDisplay();
+			
 			    break; }
 			
 			
 			default: 
 				ja.append("\n Please choose a valid option from the menu \n");
-				menuDisplay();
+				
                
 			
 			}
@@ -310,16 +311,16 @@ public class GUI extends JFrame{
 	    
 	    public static void menuDisplay() 
 	    {
-	    	ja.append("\nPlease select a preference list and then choose an option from the menu \n"
-					 +	"Enter 1 to generate a new candidate solution \n"
-					 +   "Enter 2 to print out the candidate solution \n"
-					 +   "Enter 3 to calculate the fitness and energy of that solution \n"
-					 +    "Enter 4 to make a random change to the solution \n"
-					 +    "Enter 5 to use hill climbing to improve the solution \n"
-					 +    "Enter 6 to use Simulated Annealing to improve the solution \n"
-					 +    "Enter 7 to use Genetic Algorithms to improve the solution \n"
-					 +    "Enter 8 To see which files you currently have loaded \n"
-					 +    "Enter 9 To save your file \n"
+	    	jb.append("Please select a preference list and then choose an option from the menu \n"
+					 +"Enter 1 to generate a new candidate solution \n"
+					 + "Enter 2 to print out the candidate solution \n"
+					 + "Enter 3 to calculate the fitness and energy of that solution \n"
+					 + "Enter 4 to make a random change to the solution \n"
+					 + "Enter 5 to use hill climbing to improve the solution \n"
+					 + "Enter 6 to use Simulated Annealing to improve the solution \n"
+					 + "Enter 7 to use Genetic Algorithms to improve the solution \n"
+					 + "Enter 8 To see which files you currently have loaded \n"
+					 + "Enter 9 To save your file \n"
 					 );
 	    
 	    }
