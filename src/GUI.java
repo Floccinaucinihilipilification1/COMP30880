1fc3e00e384da9e1e64b160788554158087b2329
pseudonym55 <bluemagnusstormcrow@gmail.com>
2020-05-15 05:29:41 +0100

    Removed switch state menu

--- src/GUI.java
+++ src/GUI.java
@@ -39,9 +39,7 @@ public class GUI extends JFrame{
 	private JButton reg9;
 	static JPanel jp = new JPanel(new BorderLayout(20,20));
 	static JPanel jp2 = new JPanel(new BorderLayout(20,20));
-	static JTextField jt = new JTextField("Enter your choice here",60);
 	static JTextArea ja = new JTextArea(10, 10);
-	static JTextArea jb = new JTextArea(10, 10);
 	static File file;
 	static String input;
 	
@@ -57,40 +55,22 @@ public class GUI extends JFrame{
 		 
 	        MenuBar();
 	        setTitle("Call of Cthulu Project Management System");
-	        setSize(1200, 800);
+	        setSize(1700, 1000);
 	        setLocationRelativeTo(null);
 	        setDefaultCloseOperation(EXIT_ON_CLOSE);
-	        text(solution);
 	        menuDisplay();
 	        buttons();
 	        add(jp);
-	        
 	        statusBar = new JLabel("Choose a button");
 	        statusBar.setBorder(BorderFactory.createEtchedBorder());
 	        add(statusBar, BorderLayout.SOUTH);
 	    }
 
-	    private static void text(CandidateSolution solution) {
-	    jp.add(jt, BorderLayout.SOUTH);
-	    
-	    
-	    jt.addActionListener(new ActionListener() {
 
-			@Override
-			public void actionPerformed(ActionEvent e) 
-			{
-			     input = null;
-				input = jt.getText();
-		        menuChoice();
-			}
-
-	    	
-	    });
 	    
 	    
 	    	
 		
-	}
 	    
 	    private void buttons() 
 	    {
@@ -250,12 +230,10 @@ public class GUI extends JFrame{
 	        iconExit = new ImageIcon("src/exit.png");
 	        JFileChooser fileChooser = new JFileChooser(); //This method will search all type of files, not only CSV/TSV
 	        jp.add(ja, BorderLayout.WEST); //This adds text area to GUI
-	        jp.add(jb, BorderLayout.EAST);
 	        jp.add(jp2,BorderLayout.NORTH);
 	        JScrollPane scrollPane1 = new JScrollPane(ja);
 	        scrollPane1.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 	        ja.setEditable(false);
-	        jb.setEditable(false);
 	        jp.add(scrollPane1);
 	        
 	        
@@ -349,154 +327,19 @@ public class GUI extends JFrame{
 	        }
 	    }
 
-	    public static void menuChoice() 
-		 {   
-	    	
-	    	x = 0;
-			 
-		
-			 
-			 try {
-					x = Integer.parseInt(input);
-				} catch (Exception e) {
-					ja.append("\n Please enter a valid input \n");
-				}
-		 
-		 
-		 switch(x) 
-		 {
-		 
-		 case 1: 
-			 if(GUI.y == null) {ja.append("\n No preference list loaded. Please load a Preference list \n"); break;}
-			 else { 
-			 GUI.solution = cand.generate();
-			   ja.append("\n You typed 1. Solution Generated \n");
-			   ja.append("The size of the solution is" + GUI.solution.getSize() + "\n");
-               
-			   break; }
-			
-		 case 2:
-			 if(GUI.solution == null) {ja.append("\n No solution generated. Please generate a solution first \n"); break;}
-			 else {
-				 ja.append("\n" + GUI.solution.toString());
-				 ja.append("\n You typed 2. Solution Printed. \n");
-		
-				break; }
-			
-			
-		case 3:
-			if(GUI.solution == null) {ja.append("\n No solution generated. Please generate a solution first \n"); break;}
-			 else {
-			ja.append("\n You typed 3.\n");
-			ja.append("The Fitness of the solution is " + SolutionChanger.Fitness(GUI.solution));
-			ja.append("\n" + "The Energy of the solution is " + SolutionChanger.Energy(GUI.solution)+ "\n");
-			
-	
-				break; }
-			
-		case 4:
-			if(GUI.solution == null) {ja.append("\n No solution generated. Please generate a solution first \n"); break;}
-			 else {
-			SolutionChanger.changeRandom(GUI.solution);
-			ja.append("\n You typed 4. Random change applied to solution. \n");
-			ja.append("The Energy of the solution is: " + SolutionChanger.Energy(GUI.solution)+ "\n");
-
-			
-             break; }
-          
-		case 5:
-			if(GUI.solution == null) {ja.append("\n No solution generated. Please generate a solution first \n"); break;}
-			 else {
-			HillClimbingWithSA.climbing(GUI.solution);
-			GUI.solution.saveSolution("C:\\Users\\Public\\OptimumSolutionHillClimbing.csv");
-			ja.append("\n You typed 5. Hill Climbing implemented to solution. \n");
-			ja.append(SolutionChanger.changeRandom(GUI.solution));
-			ja.append("\n\n Hill Climbing solution is saved at C:\\Users\\Public\\OptimumSolutionHillClimbing.csv \n");
-			ja.append("The Energy of the solution is " + SolutionChanger.Energy(GUI.solution)+ "\n");
-			
-		
-             break; }
-			
-			
-		case 6: 
-			if(GUI.solution == null) {ja.append("\n No solution generated. Please generate a solution first \n"); break;}
-			 else {
-				 GUI.solution = HillClimbingWithSA.climbing(GUI.solution);
-				 GUI.solution.saveSolution("C:\\Users\\Public\\OptimumSolutionSA.csv");
-				 ja.append("\n You typed 6. Simulated Annealing implemented to solution. \n");
-				 ja.append("SA solution is saved at C:\\Users\\Public\\OptimumSolutionSA.csv"+ "\n");
-				 ja.append("The Energy of the solution is " + SolutionChanger.Energy(GUI.solution)+ "\n");
-			
-			    break; }
-			
-		case 7:
-			if(GUI.solution == null) {ja.append("\n No solution generated. Please generate a solution first \n"); break;}
-			 else {
-				 GUI.solution = GeneticAlgorithm.GeneticAlgorithmGeneration(GUI.cand);
-				 GUI.solution.saveSolution("C:\\Users\\Public\\OptimumSolutionGA.csv");
-				 ja.append("\n You typed 7. Genetic Annealing implemented to solution. \n");
-				 ja.append("GA solution is saved at C:\\Users\\Public\\OptimumSolutionGA.csv"+ "\n");
-				 ja.append("The fitness of the solution is " + SolutionChanger.Fitness(GUI.solution)+ "\n");
-			
-			    break; }
 	    
-		case 8:
-			if(GUI.file == null) {ja.append("\n" + "No files have been loaded"  + "\n"); break;}
-			 else {
-			ja.append("\n" + "The following preference list has been loaded" + GUI.file.getAbsolutePath() + "\n");	 
-			
-			    break; }
-			
-		case 9:
-			if(GUI.file == null) {ja.append("\n" + "No files have been loaded"  + "\n"); break;}
-			 else {
-				 GUI.solution.saveSolution("C:\\Users\\Public\\OptimumSolution.csv"); 
-				 ja.append("\n" + "File Saved"  + "\n");
-				 ja.append("\n" + "The file has been saved to the following location C:\\Users\\Public\\OptimumSolution.csv "  + "\n");
-			
-			    break; }
-			
-			
-			default: 
-				ja.append("\n Please choose a valid option from the menu \n");
-				
-               
-			
-			}
 			 
 			 
 			 
 			 
 
-		 }
 	    
 	    public static void menuDisplay() 
 	    {
 
-	    	ja.append("\nPlease load a preference list and then choose an option from the menu \n"
-	    			+ "The preference list should be in the csv format \n"
-					 +	"Enter 1 to generate a new candidate solution \n"
-					 +   "Enter 2 to print out the candidate solution \n"
-					 +   "Enter 3 to calculate the fitness and energy of that solution \n"
-					 +    "Enter 4 to make a random change to the solution \n"
-					 +    "Enter 5 to use hill climbing to improve the solution \n"
-					 +    "Enter 6 to use Simulated Annealing to improve the solution \n"
-					 +    "Enter 7 to use Genetic Algorithms to improve the solution \n"
-					 +    "Enter 8 To see which files you currently have loaded \n"
-					 +    "Enter 9 To save your file \n");
+	    	ja.append("\nPlease load a preference list and then choose a button from the menu above \n"
+	    			+ "The preference list loaded should be in the csv format \n" );
 
-	    	jb.append("Please select a preference list and then choose an option from the menu \n"
-					 +"Enter 1 to generate a new candidate solution \n"
-					 + "Enter 2 to print out the candidate solution \n"
-					 + "Enter 3 to calculate the fitness and energy of that solution \n"
-					 + "Enter 4 to make a random change to the solution \n"
-					 + "Enter 5 to use hill climbing to improve the solution \n"
-					 + "Enter 6 to use Simulated Annealing to improve the solution \n"
-					 + "Enter 7 to use Genetic Algorithms to improve the solution \n"
-					 + "Enter 8 To see which files you currently have loaded \n"
-					 + "Enter 9 To save your file \n"
-
-					 );
 	    
 	    }
 	    
